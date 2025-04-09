f6c98981a9d03b3777e961fc72661c31b2ebbac6
Scott-Jeppson <sfjepp46@gmail.com>
2025-04-09 11:15:20 -0600

    Making models for people and groups

--- _server/core/models.py
+++ _server/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=255)
+    notes = models.JSONField(null=True, blank=True)
+
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    members = models.ManyToManyField(Person, related_name='groups')
