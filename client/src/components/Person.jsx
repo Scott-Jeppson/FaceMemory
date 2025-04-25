2a62a3365942135ee85245088af9570d3df425c0
Scott-Jeppson <sfjepp46@gmail.com>
2025-04-24 22:37:54 -0600

    Fixed .slice bug

--- client/src/components/Person.jsx
+++ client/src/components/Person.jsx
@@ -9,25 +9,38 @@ export function Person({
     <img src={person.image ? `http://localhost:8000/media/${person.image}` : '/assets/default-image.png'} alt={person.name} />
     <h2>{person.name}</h2>
     <h3>{person.notes ? "Details" : "No Details"}</h3>
-    {Array.isArray(person.notes) && detailed ? person.notes.map((note) => (
-        <p key={note.id} className="note">
-            {note.text}
-        </p>
-    )) : person.notes.slice(0,5).map((note) => (
-        <p key={note.id} className="note">
-            {note.text}
-        </p>
-    ))}
-    <h3>{person.groups ? "Groups" : "Not in Any Groups"}</h3>
-    {Array.isArray(person.groups) && detailed ? person.groups.map((group) => (
-        <p key={group.id} className="group">
-            {group.name}
-        </p>
-    )) : person.groups.slice(0,5).map((group) => (
-        <p key={group.id} className="group">
-            {group.name}
-        </p>
-    ))}
+{Array.isArray(person.notes) ? (
+    detailed ? (
+        person.notes.map((note) => (
+            <p key={note.id} className="note">
+                {note.text}
+            </p>
+        ))
+    ) : (
+        person.notes.slice(0, 5).map((note) => (
+            <p key={note.id} className="note">
+                {note.text}
+            </p>
+        ))
+    )
+) : null}
+
+<h3>{person.groups ? "Groups" : "Not in Any Groups"}</h3>
+{Array.isArray(person.groups) ? (
+    detailed ? (
+        person.groups.map((group) => (
+            <p key={group.id} className="group">
+                {group.name}
+            </p>
+        ))
+    ) : (
+        person.groups.slice(0, 5).map((group) => (
+            <p key={group.id} className="group">
+                {group.name}
+            </p>
+        ))
+    )
+) : null}
   </>
   );
 }
