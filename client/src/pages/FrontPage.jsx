ee4ba96fada2b6738488a416678562b150c497a1
Scott-Jeppson <sfjepp46@gmail.com>
2025-04-29 16:51:02 -0600

    Made front page functional

--- client/src/pages/FrontPage.jsx
+++ client/src/pages/FrontPage.jsx
@@ -1,5 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Search } from "../components/ItemSearch";
+import { useState, useEffect } from "react";
+import { getUser } from "../utils/getUser";
+import { Person } from "../components/Person";
+import { Group } from "../components/Group";
+
 export function FrontPage() {
+    const [user, setUser] = useState(null);
+    const [people, setPeople] = useState([]);
+    const [filteredPeople, setFilteredPeople] = useState([]);
+    const [peopleSearchTerm, setPeopleSearchTerm] = useState("");
+    const [groups, setGroups] = useState([]);
+    const [filteredGroups, setFilteredGroups] = useState([]);
+    const [groupsSearchTerm, setGroupsSearchTerm] = useState("");
+
+    useEffect(() => {
+        async function fetchData() {
+            const user = await getUser();
+            setUser(user);
+            const peopleResponse = await fetch("http://localhost:8000/people");
+            const peopleData = await peopleResponse.json();
+            setPeople(peopleData);
+            const groupsResponse = await fetch("http://localhost:8000/groups");
+            const groupsData = await groupsResponse.json();
+            setGroups(groupsData);
+        }
+        fetchData();
+    }, []);
+
     return (
-        <h1>This is the Front Page!</h1>
+        <div className="pageContent">
+            <div className="peopleSection">
+                <h2>Recent People</h2>
+                {people.slice(0, 10).map((person) => (
+                    <Link to={`/people/${person.id}`} key={person.id}>
+                        <Person person={person} />
+                    </Link>
+                ))}
+            </div>
+            <div className="groupSection">
+                <h2>Recent Groups</h2>
+                {groups.slice(0, 10).map((group) => (
+                    <Link to={`/groups/${group.id}`} key={group.id}>
+                        <Group group={group} />
+                    </Link>
+                ))}
+            </div>
+        </div>
     )
 }
